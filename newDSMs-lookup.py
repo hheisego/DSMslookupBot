82df2d27385d90c496841e6330b77ce71adbcfd7
hheisego <hheisego@gmail.com>
2021-09-21 10:34:11 -0500

    Initial test query

--- newDSMs-lookup.py
+++ newDSMs-lookup.py
@@ -1,4 +1,44 @@
-from core_au import user, pwd, fixed_URL
+import json
+import re
+import requests
+import time
+#from datetime import datetime, date
+import datetime
+from core_au import user, pwd, fixed_url
+
+start_time = time.perf_counter()
+
+class newDSMlookup:
+    # Set proper headers
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+
+    def expiryChecker(self, contractExp):
+
+        try:
+
+            contractExp = datetime.datetime.strptime(contractExp, "%m/%d/%Y")
+            todaysDate = datetime.datetime.now()
+
+            result = str((contractExp - todaysDate).days)
+
+            return result
+
+        except:
+
+            return "\n Could not calculate the service remaining days..."
+
+    def chorus(self, account_name):
+
+        final_url = fixed_url + "%2cu_cs_case_owner%2cu_offer_name%2Ccustomer%2cu_order_status%2cu_order_substatus%2cu_offer_type%2Cu_te_primary%2Cu_te_primary.email%2Cu_te_secondary%2Cu_te_secondary.email%2Cu_te_tertiary%2Cu_te_tertiary.email%2Cu_contract_number%2Cu_contract_term%2Cu_service_start_date%2Cu_service_end_date%2Cu_subscription_id%2Cu_web_order_id%2Cu_architecture%2Cu_covered_products%2Cu_covered_product%2Cu_product_status&sysparm_query=customerLIKE" + account_name #+ '^u_order_status!=Expired^u_order_sub_status!=Inactive'  #"  # ^u_order_substatus!=Expired"
+
+        # Do the HTTP request
+        response = requests.get(final_url, auth=(user, pwd), headers=self.headers)
+
+        expert = response.json()["result"]
+
+        return expert
 
 
-print(user)
+dsmlookup = newDSMlookup()
+
+print(dsmlookup.chorus(account_name="t-mobile"))
