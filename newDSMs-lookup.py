0c75a6b3e9a561f1785f84edb17989622a2dcd51
hheisego <hheisego@gmail.com>
2021-09-30 17:49:52 -0500

    getDSM-1

--- newDSMs-lookup.py
+++ newDSMs-lookup.py
@@ -36,7 +36,8 @@ class newDSMlookup:
 
     def chorus(self, account_name):
 
-        final_url = fixed_url + "%2Cu_record_number%2cu_cs_case_owner%2cu_offer_name%2Ccustomer%2cu_order_status%2cu_order_substatus%2cu_offer_type%2Cu_te_primary%2Cu_te_primary.email%2Cu_te_secondary%2Cu_te_secondary.email%2Cu_te_tertiary%2Cu_te_tertiary.email%2Cu_contract_number%2Cu_contract_term%2Cu_service_start_date%2Cu_service_end_date%2Cu_subscription_id%2Cu_web_order_id%2Cu_architecture%2Cu_covered_products%2Cu_covered_product%2Cu_product_status&sysparm_query=customerLIKE" + account_name #+ '^u_order_status!=Expired^u_order_sub_status!=Inactive'  #"  # ^u_order_substatus!=Expired"
+        #previous query parameters: "%2Cu_record_number%2cu_cs_case_owner%2cu_offer_name%2Ccustomer%2cu_order_status%2cu_order_substatus%2cu_offer_type%2Cu_te_primary%2Cu_te_primary.email%2Cu_te_secondary%2Cu_te_secondary.email%2Cu_te_tertiary%2Cu_te_tertiary.email%2Cu_contract_number%2Cu_contract_term%2Cu_service_start_date%2Cu_service_end_date%2Cu_subscription_id%2Cu_web_order_id%2Cu_architecture%2Cu_covered_products%2Cu_covered_product%2Cu_product_status&sysparm_query=customerLIKE"
+        final_url = fixed_url + "%2Cu_record_number%2cu_cs_case_owner%2cu_offer_name%2Ccustomer%2cu_order_status%2cu_order_substatus%2cu_offer_type%2Cu_contract_number%2Cu_contract_term%2Cu_service_start_date%2Cu_service_end_date%2Cu_subscription_id%2Cu_web_order_id%2Cu_architecture%2Cu_covered_products%2Cu_covered_product%2Cu_product_status&sysparm_query=customerLIKE" + account_name #+ '^u_order_status!=Expired^u_order_sub_status!=Inactive'  #"  # ^u_order_substatus!=Expired"
 
         # Do the HTTP request
         expert = requests.get(final_url, auth=(user, pwd), headers=self.headers).json()["result"]
@@ -101,7 +102,7 @@ class newDSMlookup:
                     collab_products, security_info, inserted_values = [], [], []
                     collaboration = []
                     subscriptions = []
-                    offer_t = []
+                    last_updates = []
                     weborders = []
                     contracts = []
                     offer_n = []
@@ -114,12 +115,7 @@ class newDSMlookup:
                             if data['u_architecture'] == 'Collaboration':
 
                                 info = {}
-                                info['dsm1'] = data['u_te_primary']
-                                info['email1'] = data['u_te_primary.email']
-                                info['dsm2'] = data['u_te_secondary']
-                                info['email2'] = data['u_te_secondary.email']
-                                info['dsm3'] = data['u_te_tertiary']
-                                info['email3'] = data['u_te_tertiary.email']
+                                info['Customer'] = data['customer']
                                 info['service_start'] = data['u_service_start_date']
                                 info['service_end'] = data['u_service_end_date']
                                 info['order_status'] = data['u_order_substatus']
@@ -128,11 +124,36 @@ class newDSMlookup:
                                 print(data['number'])
                                 print(self.getDSMs(svr_number=data['number']))
 
-            return collaboration
+                                for i in self.getDSMs(svr_number=data['number']):
+
+                                    if i.get("u_role") == "TE Primary" and i.get('u_last_updated') is not None:
+
+                                        ultima_modificacion = i.get('u_last_updated')[:i.get('u_last_updated').index(" ")]
+                                        ultima_modificacion = datetime.datetime.strptime(ultima_modificacion, "%m/%d/%Y")
+
+                                        last_updates.append(ultima_modificacion)
+
+
+                                print("\nahi va la chida:\n")
+
+                                ultima_modificacion = str(max(last_updates))
+                                ultima_modificacion = ultima_modificacion[:ultima_modificacion.index(" ")]
+                                ultima_modificacion = datetime.datetime.strptime(ultima_modificacion, "%Y-%m-%d")
+                                ultima_modificacion = ('{0}/{1}/{2}'.format(ultima_modificacion.month, ultima_modificacion.day, ultima_modificacion.year))
+
+                                print(ultima_modificacion)
+
+                                for i in self.getDSMs(svr_number=data['number']):
+
+                                    if i.get('u_last_updated') is not None and i.get('u_last_updated').find(ultima_modificacion) == True:
+
+                                        print(i.get("u_technical_expert"))
+
+            return "simon"
 
 
 dsmlookup = newDSMlookup()
 
 print(dsmlookup.chorus(account_name="t-mobile"))
-
+print("\n\n")
 print(time.perf_counter() - start_time, "seconds")
